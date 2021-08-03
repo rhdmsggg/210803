92103bba7cf021911f4073143bc2a36829b4f7ae
rhdmsggg <rhdmsggg@naver.com>
2021-08-03 15:53:55 +0900

    함수1

---
+++ BasicSet.py
@@ -0,0 +1,21 @@
+#BasicSet.py
+languages = {'C++','JAVA','Python','Python'}
+print(languages)
+
+color = set()
+print(color)
+
+color_list = []
+print(color_list)
+
+color = set()
+color.add('red')
+color.add('blue')
+color.add('gray')
+print(color)
+
+color.add('red')
+print(color)
+
+print('red' in color)
+print('black' in color)
